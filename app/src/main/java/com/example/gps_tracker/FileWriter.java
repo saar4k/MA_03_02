28e482be5f742da620fe3f058eaff9ec75b0c220
Dominik Reimer <s87498@bht-berlin.de>
2023-05-23 07:33:31 +0200

    fertig

--- app/src/main/java/com/example/gps_tracker/FileWriter.java
+++ app/src/main/java/com/example/gps_tracker/FileWriter.java
@@ -13,9 +13,10 @@ import java.util.Date;
 import java.util.Locale;
 
 class FileWriter {
-    Boolean m_bIsPause = false;
-    FileOutputStream mFile = null;
-    Context mContext;
+    private Boolean m_bIsPause = false;
+    private FileOutputStream mGpxFile = null;
+    private FileOutputStream mCsvFile = null;
+    private Context mContext;
 
     FileWriter(Context context) {
         mContext = context;
@@ -26,79 +27,22 @@ class FileWriter {
     }
 
     void startRecording() {
-        // if there's paused state just end this state
         if(m_bIsPause == true) {
             m_bIsPause = false;
             return;
         }
         try {
-            mFile = new FileOutputStream(createNewFile());
+            mGpxFile = new FileOutputStream(createNewFile(".gpx"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        write("<?xml version=\"1.0\"?>");
-        write("<gpx version=\"1.1\" creator=\"Android Smartphone\">");
-        write("<name>Trackname1</name>");
-        write("<desc>Trackbeschreibung</desc>");
-        write("<trk>");
-        write("<trkseg>");
+        write("<?xml version=\"1.0\"?>", mGpxFile);
+        write("<gpx version=\"1.1\" creator=\"Android Smartphone\">", mGpxFile);
+        write("<name>Trackname1</name>", mGpxFile);
+        write("<desc>Trackbeschreibung</desc>", mGpxFile);
+        write("<trk>", mGpxFile);
+        write("<trkseg>", mGpxFile);
         log("startRecording successfull");
-
-    }
-
-    void pauseRecording() {
-        // enter paused state
-        m_bIsPause = true;
-    }
-
-    void stopRecording() {
-        // end paused state
-        m_bIsPause = false;
-        try {
-            if(mFile != null) {
-                write("</trkseg>");
-                write("</trk>");
-                write("</gpx>");
-                mFile.close();
-                mFile = null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        log("stopRecording succesfull");
-    }
-
-    void write(String text) {
-        text += "\r\n";
-        try {
-            mFile.write(text.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void writePosition(double longitude, double latitude, double altitude) {
-        // only write to file if there's no paused state
-        if(m_bIsPause == true)
-            return;
-        // also return if there's no mFile-Objekt
-        if(mFile == null)
-            return;
-
-        String time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(new Date());
-        StringBuilder time = new StringBuilder(time1);
-        time.setCharAt(10,'T');
-        time.setCharAt(19,'Z');
-        write(String.format("<trkpt lat=\"%.6f\" lon=\"%.6f\">", latitude, longitude));
-        write(String.format("<ele>%.0f</ele>", altitude));
-        write(String.format("<time>%s</time>", time));
-        write("</trkpt>");
-        //log("wrote position");
-
-    }
-
-    boolean isRecording() {
-        return mFile != null;
     }
 
     void startRecordingCSV() {
@@ -107,51 +51,83 @@ class FileWriter {
             return;
         }
         try {
-            mFile = new FileOutputStream(createNewFile(".csv"));
-            write("Time,Latitude,Longitude,Altitude,Speed\n"); // CSV headers
+            mCsvFile = new FileOutputStream(createNewFile(".csv"));
+            write("Time,Latitude,Longitude,Altitude,Speed\n", mCsvFile); // CSV headers
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         log("startRecordingCSV successfull");
     }
 
-    void writePositionCSV(double longitude, double latitude, double altitude, float speed) {
-
-        if(m_bIsPause == true)
-            return;
-
-        if(mFile == null)
-            return;
-
-        String time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        write(String.format(Locale.US,"%s,%.6f,%.6f,%.2f,%.2f\n", time1, latitude, longitude, altitude, speed));
-        //log("wrote position");
+    void pauseRecording() {
+        m_bIsPause = true;
     }
 
+    void resumeRecording() {
+        m_bIsPause = false;
+    }
 
-    private File createNewFile() {
-        File fileStorageDir = null;
-        //if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-        //    fileStorageDir = mContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-        //}
+    void stopRecording() {
+        m_bIsPause = false;
+        try {
+            if(mGpxFile != null) {
+                write("</trkseg>", mGpxFile);
+                write("</trk>", mGpxFile);
+                write("</gpx>", mGpxFile);
+                mGpxFile.close();
+                mGpxFile = null;
+            }
+            if(mCsvFile != null) {
+                mCsvFile.close();
+                mCsvFile = null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log("stopRecording successful");
+    }
 
-        fileStorageDir = mContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+    void write(String text, FileOutputStream file) {
+        text += "\r\n";
+        try {
+            file.write(text.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File file = new File(fileStorageDir.getPath() + File.separator + timeStamp+ ".gpx");
-        return file;
+    void writePosition(double longitude, double latitude, double altitude) {
+        if(m_bIsPause) return;
+        if(mGpxFile == null) return;
+
+        String time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(new Date());
+        StringBuilder time = new StringBuilder(time1);
+        time.setCharAt(10,'T');
+        time.setCharAt(19,'Z');
+        write(String.format(Locale.US,"<trkpt lat=\"%.6f\" lon=\"%.6f\">", latitude, longitude), mGpxFile);
+        write(String.format(Locale.US,"<ele>%.0f</ele>", altitude), mGpxFile);
+        write(String.format(Locale.US,"<time>%s</time>", time), mGpxFile);
+        write("</trkpt>", mGpxFile);
+    }
+
+    void writePositionCSV(double longitude, double latitude, double altitude, float speed) {
+        if(m_bIsPause) return;
+        if(mCsvFile == null) return;
+
+        String time1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        write(String.format(Locale.US,"%s,%.6f,%.6f,%.2f,%.2f\n", time1, latitude, longitude, altitude, speed), mCsvFile);
+    }
+
+    boolean isRecording() {
+        return mGpxFile != null || mCsvFile != null;
     }
 
     private File createNewFile(String extension) {
         File fileStorageDir = mContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File file = new File(fileStorageDir.getPath() + File.separator + timeStamp + extension);
         Log.d("FileWriter", "File path: " + file.getAbsolutePath());
         return file;
     }
-
-
-
-
 }
+
--- app/src/main/java/com/example/gps_tracker/MainActivity.java
+++ app/src/main/java/com/example/gps_tracker/MainActivity.java
@@ -4,15 +4,21 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +32,7 @@ import androidx.core.content.ContextCompat;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -33,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     private static final int PERMISSIONS_REQUEST_LOCATION = 1;
     private static final int PERMISSIONS_REQUEST_WRITE_STORAGE = 2;
-
+    private ArrayList<UTMRef> utmPoints = new ArrayList<>();
     private LocationManager locationManager;
     private TextView hoehenValueTextView, speedValueTextView;
     private TextView latitudeValueTextView, longitudeValueTextView;
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        // Überprüfen, ob die Berechtigung zur Standortabfrage erteilt wurde
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
         } else {
@@ -76,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Standortabfrage wurde erteilt
+
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
@@ -101,13 +108,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         double longitude = location.getLongitude();
         double altitude = location.getAltitude();
         float speed = location.getSpeed();
-
-        // Überprüfen Sie die Berechtigung und starten Sie die Aufnahme, wenn dies noch nicht geschehen ist
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             if (!fileWriter.isRecording()) {
                 fileWriter.startRecordingCSV();
+                fileWriter.startRecording();
             }
             fileWriter.writePositionCSV(longitude, latitude, altitude, speed);
+            fileWriter.writePosition(longitude, latitude, altitude);
+
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_WRITE_STORAGE);
         }
@@ -121,6 +129,45 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         hoehenValueTextView.setText(String.format("%.2f m", altitude));
         speedValueTextView.setText(String.format("%.2f km/h", speed));
+
+        // Konvertierung von GPS in UTM
+        System.out.println("Convert Latitude/Longitude to UTM Reference");
+        LatLng ll4 = new LatLng(location.getLatitude(), location.getLongitude());
+        System.out.println("Latitude/Longitude: " + ll4.toString());
+        UTMRef utm2 = ll4.toUTMRef();
+        System.out.println("Converted to UTM Ref: " + utm2.toString());
+        System.out.println();
+        utmPoints.add(utm2);
+        ImageView imageView=(ImageView) findViewById(R.id.imageView);
+        drawRouteAndGrid(imageView, utm2);
+    }
+
+    private void drawRouteAndGrid(ImageView imageView, UTMRef utm2) {
+        int imageViewWidth = imageView.getWidth();
+        int imageViewHeight = imageView.getHeight();
+        Bitmap bitmap = Bitmap.createBitmap(imageViewWidth, imageViewHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+
+        // Zeichnen der Hilfslinien
+        for (int i = 0; i < 5; i++) {
+            canvas.drawLine(0, i * imageViewHeight / 5, imageViewWidth, i * imageViewHeight / 5, paint);
+            canvas.drawLine(i * imageViewWidth / 5, 0, i * imageViewWidth / 5, imageViewHeight, paint);
+        }
+
+        for (UTMRef point : utmPoints) {
+            float x = (float) ((point.getEasting() % 2500) / 2500 * imageViewWidth);
+            float y = (float) ((1 - (point.getNorthing() % 2500) / 2500) * imageViewHeight);
+
+
+
+            int color = Color.rgb(255, 150, 79);
+            paint.setColor(color);
+            canvas.drawCircle(x, y, 10, paint);
+        }
+
+        imageView.setImageBitmap(bitmap);
     }
 
 
@@ -175,5 +222,29 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             e.printStackTrace();
             Toast.makeText(this, "Fehler beim Konvertieren der Koordinaten", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        fileWriter.stopRecording();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        fileWriter.stopRecording();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fileWriter.pauseRecording();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fileWriter.resumeRecording();
     }
 }
